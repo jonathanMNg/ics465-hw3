3b88f81a7b52aca2f8e2a352dab0f16cba7ab839
jonathanmng <31453488+jonathanMNg@users.noreply.github.com>
2018-04-08 01:40:45 -1000

    update ui

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 class Square extends React.Component{
   render() {
     let red = this.props.winner ? ' red' : '';
     return (
-        <button className={"square" + red} onClick={() => this.props.onClick()}>
+        <button className={"square" + red } onClick={() => this.props.onClick()}>
           {this.props.value}
         </button>
     );
@@ -74,30 +73,49 @@ class Game extends React.Component {
           'Go to game start';
       return (
           <li key={move}>
-            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            <button className="ui inverted black button step" onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
       );
     });
     let status;
+    let restart;
+    let restartClassName;
+    let statusClassName;
     if (winner) {
-      status = 'Winner: ' + winner[0];
-      current.squares.winSquares = winner[3];
+      if(winner === 'draw') {
+        status = 'Draw!';
+        statusClassName = "ui red message";
+        restart = 'Click on the board again to restart';
+        restartClassName = "ui info message";
+      }
+      else {
+        status = 'Winner: ' + winner[0];
+        statusClassName = "ui green message";
+        restart = 'Click on the board again to restart';
+        current.squares.winSquares = winner[3];
+        restartClassName = "ui info message";
+      }
     }
     else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
     return (
-        <div className="game">
-        <div className="game-board">
-        <Board
-          squares={current.squares}
-          onClick={(i) => this.handleClick(i)}
-        />
-        </div>
-        <div className="game-info">
-        <div>{status}</div>
-        <ol>{moves}</ol>
-        </div>
+        <div className="ui container game">
+          <div className="ui grid">
+            <div className="centered row">
+              <div id="game-board" className="game-board">
+                <Board
+                    squares={current.squares}
+                    onClick={(i) => this.handleClick(i)}
+                />
+              </div>
+              <div className="game-info">
+                <div className={statusClassName}>{status}</div>
+                <div className={restartClassName}>{restart}</div>
+                <ol>{moves}</ol>
+              </div>
+            </div>
+          </div>
         </div>
     );
   }
@@ -107,6 +125,9 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if(calculateWinner(squares) || squares[i]) {
+      if(calculateWinner(squares)) {
+        this.jumpTo(0);
+      }
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -135,6 +156,9 @@ function calculateWinner(squares) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return [squares[a], squares[b], squares[c], lines[i]];
+    }
+    else if(!squares.includes(null)){
+      return 'draw';
     }
   }
   return null;
